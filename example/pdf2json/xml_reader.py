e5db8dc9f7b118dc998b20f8937d36c810fdaf01
ChangLijie <lichieh_chang@innodisk.com>
2025-06-10 16:42:52 +0800

    feat : add pytantic.

--- example/pdf2json/xml_reader.py
+++ example/pdf2json/xml_reader.py
@@ -8,4 +8,8 @@ if __name__ == "__main__":
     path = "./data/EMPU_3401_Datasheet/EMPU_3401_Datasheet.xml"
     xml_parser = XMLParser()
 
-    print(xml_parser.process(path=path))
+    xml_data = xml_parser.process(path=path)
+    # print(len(xml_data.pages))
+    # print(xml_data.pages)
+    # print(xml_data.pages[1].size)
+    # print(xml_data.pages[1].data[1])
